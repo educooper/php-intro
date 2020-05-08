f471cd4434679dce10a5deca427706b2de819f94
educooper <edusardo@uninove.edu.br>
2020-05-08 06:03:36 -0300

    inseridas validações de formulario

--- script.php
+++ script.php
@@ -1,7 +1,5 @@
 <?php
 
-function defineCategoriaCompetidor(string $nome, string $idade) : ?string
-{
     $catetorias = [];
     $categorias[] = 'infantil';
     $categorias[] = 'adolescente';
@@ -10,52 +8,57 @@ function defineCategoriaCompetidor(string $nome, string $idade) : ?string
 
     $nome = $_POST['nome'];
     $idade = $_POST['idade'];
+    
+    if(empty($nome)){
+        echo 'O nome não pode ser vazio';
+        return;
+    }
+    if(strlen($nome) < 3)
+    {
+        echo 'O nom deve conter mais de 3 caracteres';
+        return;
+    }
+    if(strlen($nome) > 40)
+    {
+        echo 'O nome é muito extenso';
+        return;
+    }
+    if(!is_numeric($idade))
+    {
+        echo 'Informe um numero para idade';
+        return;
+    }
+    if($idade >= 6 && $idade <= 12)
+    {
+        for($i = 0; count($categorias); i++ )
+        {
+            if($categorias[$i] == 'infantil')
+                echo "O nadador ".$nome." compete na categoria ".$categorias[$i];
+        }
+    }
+    else if($idade >= 13 && $idade <= 18)
+    {
+        for($i = 0; count($categorias); i++ )
+        {
+            if($categorias[$i] == 'adolescente')
+                echo "O nadador ".$nome." compete na categoria adolescente.";
+        }
+    }
+    else if($idade >= 19 && $idade <= 59)
+    {
+        for($i = 0; count($categorias); i++ )
+        {
+            if($categorias[$i] == 'adulto')
+                echo "O nadador ".$nome." compete na categoria adulto.";
+        }
+    }
+    else if($idade >= 60)
+    {
+        for($i = 0; count($categorias); i++ )
+        {
+            if($categorias[$i] == 'isodo')
+                echo "O nadador ".$nome." compete na categoria idoso.";
+        }
+    }
 
-    var_dump($nome);
-    var_dump($idade);
-/*
-    if(validaNome($nome) && validaIdade($idade))
-    {
-        removerMensagemErro();
-        if($idade >= 6 && $idade <= 12)
-        {
-            for($i = 0; $i <= count($categorias); $i++)
-            {
-                if($categorias[$i] == 'infantil')
-                {
-                    setarMensagemSucesso("O nadador ".$nome. " compete na categoria " .$categorias[$i]);
-                    return null;
-                }
-            }
-        }
-        else if($idade >= 13 && $idade <= 18)
-        {
-            for($i = 0; $i <= count($categorias); $i++)
-            {
-                if($categorias[$i] == 'adolescente')
-                {
-                    setarMensagemSucesso("O nadador ".$nome. " compete na categoria " .$categorias[$i]);
-                    return null;
-                }
-            }
-        }
-        else
-        {
-            for($i = 0; $i <= count($categorias); $i++)
-            {
-                if($categorias[$i] == 'adulto')
-                {
-                    setarMensagemSucesso("O nadador ".$nome. " compete na categoria " .$categorias[$i]);
-                    return null;
-                }
-            }
-        }
-    }
-    else
-    {
-        removerMensagemSucesso();
-        return obterMensagemErro();
-    }
-}
-*/
 ?>
